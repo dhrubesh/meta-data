7d3bc7797d57a2d6230f0ac0443cfdd5afcb2bf7
dhrubesh <dhrubesh97@gmail.com>
2017-06-14 19:32:26 +0000

    set-up

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
-
+const morgan = require('morgan')
+const path = require('path')
+const multer = require('multer')
 
 
 
 app.get('/', function (req, res) {
-  res.send('all set')
+    res.sendFile(path.join(__dirname+'/index.html'))
+//   res.send('all set')
 })
 
-
+app.post('/upload',function(req,res){
+    res.json()
+})
 
 app.listen(8080, function () {
   console.log('listening on port 8080....')
